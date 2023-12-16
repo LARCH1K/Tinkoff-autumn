d0c968df385ca84c5eddf41101c96fffb0159674
LARCHIK <ovchinnikov20035696@gmail.com>
2023-12-16 16:03:56 +0500

    Fix Task2Test

--- src/main/java/edu/hw11/task2/ArithmeticUtilsInterceptor.java
+++ src/main/java/edu/hw11/task2/ArithmeticUtilsInterceptor.java
@@ -1,10 +1,7 @@
 package edu.hw11.task2;
 
 public class ArithmeticUtilsInterceptor {
-    private ArithmeticUtilsInterceptor() {
-    }
-
-    public static int sum(int a, int b) {
+    public int sum(int a, int b) {
         return a * b;
     }
 }
